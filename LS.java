8f7e68dc475326e52df48ec3f144cc2e437e1370
ChoudhuryN7 <113545269+ChoudhuryN7@users.noreply.github.com>
2023-02-02 15:25:45 +0530

    committed

--- LS.java
+++ LS.java
@@ -1,6 +1,21 @@
 class LS
 {
 
+    public static int search(int arr[],int a)
+    {  
+        int i;
+       for( i=0;i<arr.length;i++)
+       {
+        if(arr[i]==a)
+        {
+            return i;
+            
+        }
+        
+       }
+       return -1;
+
+    }
     public static void main(String args[])
     {
         int arr[]= new int[]{1,2,3,45,3};
@@ -10,5 +25,10 @@ class LS
         {
             System.out.println(arr[i]);
         }
+        int x=search(arr,45);
+        if(x==-1)
+        System.out.println("element not found in the array");
+        else
+        System.out.println("found at index: "+ x);
     }
 }
